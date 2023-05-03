be13696b7515b1d20b8897cbeccbeb84ba4b5d00
Maël RHUIN <66364083+LIncorruptible@users.noreply.github.com>
2023-05-03 11:42:33 +0200

    Récupération API
    Récupérer les données de l'API dans un string :
        - Création de l'objet API

---
+++ code/BATAILLE_NAVALE/BATAILLE_NAVALE/CLASSE_PRINCIPALE.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using BIBLIOTHEQUE_INTERACTIONS_API;
+
+
+namespace BIBLIOTHEQUE_AFFICHAGE_CONSOLE
+{
+    internal class CLASSE_PRINCIPALE
+    {
+
+        static void Main(string[] args)
+        {
+            string url = "https://api-lprgi.natono.biz/api/GetConfig";
+            string key = "lprgi_api_key_2023";
+
+            API api = new API(url, key);
+
+            string json = api.RecupererData().Result;
+
+            // Afficher les données du JsonArray
+            Console.WriteLine("Affichage des données du JsonArray");
+            Console.WriteLine(json);
+
+        }
+    }
+}
---
+++ code/BATAILLE_NAVALE/Interactions/API.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BIBLIOTHEQUE_INTERACTIONS_API
+{
+    public class API
+    {
+        private string _URL, _KEY;
+
+        public API()
+        {
+            _URL= "";
+            _KEY = "";
+        }
+
+        public API(string url, string key)
+        {
+            _URL = url;
+            _KEY = key;
+        }
+
+        public string URL
+        {
+            get { return _URL; }
+            set { _URL = value; }
+        }
+
+        public string KEY
+        {
+            get { return _KEY; }
+            set { _KEY = value; }
+        }
+
+        public async Task<string> RecupererData()
+        {
+            Console.WriteLine("Récupération des données de l'API en cours...");
+            string json = string.Empty;
+            try
+            {
+                using (System.Net.WebClient wc = new System.Net.WebClient())
+                {
+                    wc.Headers.Add("x-functions-key", _KEY);
+                    json = wc.DownloadString(_URL);
+                }
+
+                Console.WriteLine("Données récupérées avec succès.");
+                return json;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur lors de la récupération des données : " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
