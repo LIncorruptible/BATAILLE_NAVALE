fb2fd7114d4c47504e74c887acd6832b321ae3d1
Maël RHUIN <66364083+LIncorruptible@users.noreply.github.com>
2023-05-03 19:12:18 +0200

    Ajout Objet PLATEAU

---
+++ code/BATAILLE_NAVALE/GameLibrary/PLATEAU.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIBLIOTHEQUE_LOGIQUE_JEU
+{
+    internal class PLATEAU
+    {
+        private GRILLE _GRILLE_JOUEUR_A, _GRILLE_JOUEUR_B;
+        private int[,] _GRILLE_ENEMI_DE_A, _GRILLE_ENEMI_DE_B;
+        private bool _WIN;
+    }
+}
