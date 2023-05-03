c75451a7009405f82feec2dfb9212aec4eb808c8
Maël RHUIN <66364083+LIncorruptible@users.noreply.github.com>
2023-05-03 11:55:16 +0200

    Ajout objet BATEAU
    Représentation OBJET Bateau issue de la structure de l'API.

---
+++ code/BATAILLE_NAVALE/GameLibrary/BATEAU.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIBLIOTHEQUE_LOGIQUE_JEU
+{
+    internal class BATEAU
+    {
+        private int _TAILLE;
+        private string _NOM;
+
+        public BATEAU()
+        {
+            _TAILLE = 0;
+            _NOM = "EAU";
+        }
+
+        public BATEAU(int taille, string nom)
+        {
+            _TAILLE = taille;
+            _NOM = nom;
+        }
+        
+        public int TAILLE
+        {
+            get { return _TAILLE; }
+            set { _TAILLE = value; }
+        }
+
+        public string NOM
+        {
+            get { return _NOM; }
+            set { _NOM = value; }
+        }
+    }
+}
