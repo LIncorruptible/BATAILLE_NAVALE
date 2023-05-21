2ef486788c63ace38d1ec5459c57eeaac3527c48
Maël RHUIN <66364083+LIncorruptible@users.noreply.github.com>
2023-05-21 11:17:52 +0200

    Ajout des tests unitaires pour LOGIQUE & API. LOGIQUE n'est pas entièrement couverte par les tests.

---
+++ code/BATAILLE_NAVALE/TESTS_UNITAIRES/BATEAUTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BIBLIOTHEQUE_LOGIQUE_JEU;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIBLIOTHEQUE_LOGIQUE_JEU.Tests
+{
+    [TestClass()]
+    public class BATEAUTests
+    {
+        [TestMethod()]
+        public void BATEAU_TEST_CONSTRUCTEUR_0()
+        {
+            // Arrange
+            int expectedTaille = 0;
+            string expectedNom = "EAU";
+
+            // Act
+            BATEAU bateau = new BATEAU();
+
+            // Assert
+            Assert.AreEqual(expectedTaille, bateau.TAILLE);
+            Assert.AreEqual(expectedNom, bateau.NOM);
+        }
+
+        [TestMethod()]
+        public void BATEAU_TEST_CONSTRUCTEUR_1()
+        {
+            // Arrange
+            int expectedTaille = 5;
+            string expectedNom = "Mon Bateau";
+
+            // Act
+            BATEAU bateau = new BATEAU(expectedTaille, expectedNom);
+
+            // Assert
+            Assert.AreEqual(expectedTaille, bateau.TAILLE);
+            Assert.AreEqual(expectedNom, bateau.NOM);
+        }
+
+    }
+}
---
+++ code/BATAILLE_NAVALE/TEST_UNI_B_AFF_CO/Tests.cs
@@ -0,0 +1,16 @@
+namespace TEST_UNI_B_AFF_CO
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
